82c99e6f54c1712ad495b9165bddfe7fa6d2d01c
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2024-12-20 11:29:44 +0100

    socket fix

--- scripts/socket/socketController.js
+++ scripts/socket/socketController.js
@@ -3,5 +3,7 @@ import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
 // Initializes the socket IO connection
 // Its important to note that during development this should be "localhost", but as soon as
 // we host the backend online on Railway, we need to change this to the correct URL
-export const socket = io("http://127.0.0.1:3000");
-export const ioSocket = socket.connect("http://127.0.0.1:3000");
+export const socket = io("riveteddybearbackend-production.up.railway.app");
+export const ioSocket = socket.connect(
+  "riveteddybearbackend-production.up.railway.app"
+);
